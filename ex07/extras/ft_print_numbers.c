89d616bfb47cc2a06699bb475e647581c644b8c0
zacka69 <joaorodrigues3099@gmail.com>
2024-10-15 15:36:13 +0200

    changes

--- ex07/extras/ft_print_numbers.c
+++ ex07/extras/ft_print_numbers.c
@@ -1,18 +1,30 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_putchar.c                                       :+:      :+:    :+:   */
+/*   ft_print_numbers.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: joao-alm <joao-alm@student.42luxembourg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/14 10:50:49 by joao-alm          #+#    #+#             */
-/*   Updated: 2024/10/14 10:51:32 by joao-alm         ###   ########.fr       */
+/*   Created: 2024/10/14 11:10:41 by joao-alm          #+#    #+#             */
+/*   Updated: 2024/10/15 15:16:54 by joao-alm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
+void	ft_putchar(char c);
 
-void	ft_putchar(char c)
+void	ft_print_numbers(void)
 {
-	write(1, &c, 1);
+	char	c;
+
+	c = '0';
+	while (c <= '9')
+		ft_putchar(c++);
 }
+/*
+int	main(void)
+{
+	ft_print_numbers();
+	ft_putchar('\n');
+	return (0);
+}
+*/
